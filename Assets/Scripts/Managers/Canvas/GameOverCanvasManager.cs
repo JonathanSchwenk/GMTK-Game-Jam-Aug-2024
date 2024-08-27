82e70b1e4155e3848fea66941503b4c5d7d89e5f
JonathanSchwenk <97307560+JonathanSchwenk@users.noreply.github.com>
2024-08-27 18:16:44 -0400

    Rotates camera after game ends

--- Assets/Scripts/Managers/Canvas/GameOverCanvasManager.cs
+++ Assets/Scripts/Managers/Canvas/GameOverCanvasManager.cs
@@ -5,12 +5,24 @@ using Dorkbots.ServiceLocatorTools;
 using TMPro;
 using System;
 
-public class GameOverCanvasManager : MonoBehaviour
-{
+public class GameOverCanvasManager : MonoBehaviour {
     [SerializeField] private TextMeshProUGUI yourScoreValue;
     [SerializeField] private TextMeshProUGUI bestScoreValue;
     [SerializeField] private TextMeshProUGUI roundsMaxChainValue;
     [SerializeField] private TextMeshProUGUI bestMaxChainValue;
+
+    [SerializeField] private GameObject orthoCamera;
+    [SerializeField] private GameObject perspectiveCamera;
+    [SerializeField] private GameObject cityCamera;
+
+    [SerializeField] private GameObject cityView;
+    [SerializeField] private GameObject defaultView;
+
+
+    private float radius = 180f; // The radius of the circle
+    private float speed = 0.25f;  // The speed of the movement
+    private float angle = 0f; // The current angle of rotation
+    public Vector3 targetPosition = new Vector3(100, 70, 60); // Target position (center of the circle)
 
 
     private IGamePieceManager gamePieceManager;
@@ -19,8 +31,7 @@ public class GameOverCanvasManager : MonoBehaviour
     private ISaveManager saveManager;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         gamePieceManager = ServiceLocator.Resolve<IGamePieceManager>();
         gameManager = ServiceLocator.Resolve<IGameManager>();
         audioManager = ServiceLocator.Resolve<IAudioManager>();
@@ -28,19 +39,27 @@ public class GameOverCanvasManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         yourScoreValue.text = Mathf.RoundToInt(gamePieceManager.score).ToString();
         bestScoreValue.text = Mathf.RoundToInt(saveManager.saveData.highScore).ToString();
         roundsMaxChainValue.text = Mathf.RoundToInt(gamePieceManager.curMaxChain).ToString();
         bestMaxChainValue.text = Mathf.RoundToInt(saveManager.saveData.highChain).ToString();
+
+        if (gameManager.gameState == GameState.GameOver) {
+            orthoCamera.SetActive(false);
+            perspectiveCamera.SetActive(false);
+            cityCamera.SetActive(true);
+
+            // Rotate the camera around the target
+            Rotate("right");
+        }
     }
 
     public void OnReplayPressed() {
         // Resets the game state and starts a new round
         gamePieceManager.InitRoundStats();
         gameManager.UpdateGameState(GameState.Playing);
-        
+
         audioManager.PlaySFX("UIClick_General");
         audioManager.PlaySFX("StartGame");
         audioManager.StopMusic("MenuMusic");
@@ -58,5 +77,58 @@ public class GameOverCanvasManager : MonoBehaviour
         // Returns to the main menu after resetting
         gamePieceManager.InitRoundStats();
         gameManager.UpdateGameState(GameState.StartMenu);
+
+        // Reset camera
+        orthoCamera.SetActive(true);
+        cityCamera.SetActive(false);
+    }
+
+    public void OnViewCityPressed() {
+        audioManager.PlaySFX("UIClick_General");
+        // Opens the city view
+        cityView.SetActive(true);
+        defaultView.SetActive(false);
+        
+        // Reset camera
+        orthoCamera.SetActive(true);
+        cityCamera.SetActive(false);
+    }
+
+    public void OnBackToDefaultPressed() {
+        audioManager.PlaySFX("UIClick_General");
+        // Opens the default view
+        cityView.SetActive(false);
+        defaultView.SetActive(true);
+    }
+
+    public void OnRotateLeftPressed() {
+        audioManager.PlaySFX("UIClick_Scale");
+        Rotate("left");
+    }
+
+    public void OnRotateRightPressed() {
+        audioManager.PlaySFX("UIClick_Scale");
+        Rotate("right");
+    }
+
+    private void Rotate(string direction) {
+        print("Rotating " + direction);
+        print(Time.timeScale);
+        // Calculate the new angle based on the speed
+        if (direction == "left") {
+            angle -= speed * Time.deltaTime;
+        } else if (direction == "right") {
+            angle += speed * Time.deltaTime;
+        }
+
+        // Calculate the new position of the camera
+        float x = Mathf.Cos(angle) * radius;
+        float z = Mathf.Sin(angle) * radius;
+
+        // Update the camera's position
+        cityCamera.transform.position = new Vector3(x, cityCamera.transform.position.y, z) + targetPosition;
+
+        // Make the camera look at the target
+        cityCamera.transform.LookAt(targetPosition);
     }
 }
